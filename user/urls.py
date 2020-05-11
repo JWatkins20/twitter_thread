7e665964c9aeda39c6708e6ba23c808c63a3293f
JWatkins20 <38019485+JWatkins20@users.noreply.github.com>
2020-05-10 21:30:51 -0400

    Twitter login working

---
+++ user/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path
+from django.conf.urls import url, include
+from django.contrib.auth import views as authviews
+from .views import *
+
+urlpatterns = [
+	url(r'^$', home, name='home'),
+	url(r'^login/$', authviews.LoginView.as_view(), name='login'),
+	url(r'^logout/$', authviews.LogoutView.as_view(), name='logout'),
+	url(r'^oauth/', include('social_django.urls', namespace='social')),
+]
